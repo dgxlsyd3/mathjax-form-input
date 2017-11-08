83636cea499c53b295a1ca13a2b4b133b151ce8c
zhiyuc <houshengandt@gmail.com>
2017-11-08 13:55:51 +0800

    Update forminput.js

--- unpacked/forminput.js
+++ unpacked/forminput.js
@@ -66,13 +66,9 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
       if (readonly == 'readonly') {
         var input = HTML.Element("input",{type:"text", name:id, id:id, size:size, className:cls, value:val});
         input.setAttribute("readonly", "readonly");
-        console.log(1);
       } else {
         var input = HTML.Element("input",{type:"text", name:id, id:id, size:size, className:cls, value:val});
-        console.log(2);
       }
-      console.log(readonly);
-      console.log(input);
       input.setAttribute("xmlns","http://www.w3.org/1999/xhtml");
       var mml = MML["annotation-xml"](MML.xml(input)).With({encoding:"application/xhtml+xml",isToken:true});
       this.Push(MML.semantics(mml));
